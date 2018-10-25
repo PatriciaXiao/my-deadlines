541c5ebf7268b0b3e0dd20b4af496f85db1b73de
Patricia Xiao <patricia.xiao@berkeley.edu>
2018-10-24 17:43:26 -0700

    deadline counter

--- timer.js
+++ timer.js
@@ -1,5 +1,6 @@
 function countdown_timer(date_string, container_id) {
 	var countDownDate = new Date(date_string).getTime();
+  var expired = false;
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
@@ -12,19 +13,30 @@ function countdown_timer(date_string, container_id) {
 
 	  // If the count down is finished, write some text 
 	  if (distance < 0) {
-	    clearInterval(x);
-	    document.getElementById(container_id).innerHTML = "EXPIRED";
+	    // clearInterval(x);
+	    document.getElementById(container_id).innerHTML = "<font color='gray'>EXPIRED</font>";
 	  } else {
   	  	// Time calculations for days, hours, minutes and seconds
-  	  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  	  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  	  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  	  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    	  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    	  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    	  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    	  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  	  // Display the result in the element with id="demo"
-  	  document.getElementById(container_id).innerHTML = days + "d " + hours + "h "
-  	  + minutes + "m " + seconds + "s ";
+    	  // Display the result in the element with id="demo"
+        if (days > 0 && hours >= 0 && minutes >= 0 && seconds >= 0)
+           document.getElementById(container_id).innerHTML = "<b><font color='green'>" + days + " d " + hours + " h " 
+              + minutes + " m " + seconds + " s</font></b>";
+        else if (days == 0 && hours >= 0 && minutes >= 0 && seconds >= 0)
+           document.getElementById(container_id).innerHTML = "<b><font color='red'>" + days + " d " + hours + " h " 
+              + minutes + " m " + seconds + " s</font></b>";
+        else {
+          // clearInterval(x);
+          document.getElementById(container_id).innerHTML = "<font color='gray'>EXPIRED</font>";
+        }
 	  }
 
 	}, 1000);
+
+  return expired;
+
 }
